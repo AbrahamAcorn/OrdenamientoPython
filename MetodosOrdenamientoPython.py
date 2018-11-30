742a6c5b6950165263568630a2442c7df3a9fd8e
Abraham¬_¬ <AbrahamRiRa>
2018-11-30 17:46:05 -0600

    Se agregaron los metodos faltantes

--- MetodosOrdenamientoPython.py
+++ MetodosOrdenamientoPython.py
@@ -34,6 +34,211 @@ def ordenamientoInsercion(lista):
                 i=i-1
             else:
                 break
+import time
+import random
+
+randfile = open("Random.txt", "w")
+
+start = int(input('Enter lower limit of random numbers: '))
+end = int(input('Enter upper limit of random numbers: '))
+
+for i in range(int(input('How many to generate?: '))):
+    line = str(random.randint(start, end))
+    randfile.write(line + '\n')
+    print(line)
+
+randfile.close()
+
+
+# example of selection sort algorithm : needs modification
+
+def swap(a, i, j):
+    (a[i], a[j]) = (a[j], a[i])
+
+
+
+def selectionSort(a):
+    n = len(a)
+    for startIndex in range(n):
+        minIndex = startIndex
+        for ind in range(startIndex+1, n):
+            if a[ind] < a[minIndex]:
+                minIndex = ind
+        swap(a, startIndex, minIndex)
+
+lst = []
+with open("Random.txt", "r") as f:
+    for line in f:
+        lst.append(int(line.strip()))
+
+start_time = time.time()
+selectionSort(lst)
+end_time = time.time()
+
+
+file = open("selectionSortResult","w")
+for x in lst:
+    file.write(str(x)+"\n")
+file.close()
+print('Sorted Selection Sort List: ', lst)
+print('Elapsed time for Selection Sort: {:.20f} seconds'.format(end_time-start_time))
+
+
+
+
+def merge_sort(A):
+    """
+    Sort list A into order, and return result.
+    """
+    n = len(A)
+    if n==1:
+        return A
+    mid = n//2   # floor division
+    L = merge_sort(A[:mid])
+    R = merge_sort(A[mid:])
+    return merge(L,R)
+
+def merge(L,R):
+    """
+    Given two sorted sequences L and R, return their merge.
+    """
+    i = 0
+    j = 0
+    answer = []
+    while i<len(L) and j<len(R):
+        if L[i]<R[j]:
+            answer.append(L[i])
+            i += 1
+        else:
+            answer.append(R[j])
+            j += 1
+    if i<len(L):
+        answer.extend(L[i:])
+    if j<len(R):
+        answer.extend(R[j:])
+    return answer
+
+lst = []
+# opens and reads 'Random.txt'
+with open("Random.txt", "r") as f:
+    for line in f:
+        lst.append(int(line.strip()))
+
+start_time = time.time()
+lst = merge_sort(lst)
+end_time = time.time()
+
+
+# opens and writes to file 'mergeSortResult.txt'
+file = open("mergeSortResult","w")
+for x in lst:
+    file.write(str(x)+"\n")
+# closes 'mergeSortResult.txt'
+file.close()
+print('Sorted Merge Sort List: ', lst)
+print('Elapsed time for Merge Sort: {:.20f} seconds'.format(end_time-start_time))
+def mezclaNatural(arr): 
+    if len(arr) >1: 
+        mitad = len(arr)//2 
+        aux1 = arr[:mitad]   
+        aux2 = arr[mitad:] 
+        mezclaNatural(aux1) 
+        mezclaNatural(aux2)  
+  
+        i = j = k = 0
+
+        while i < len(aux1) and j < len(aux2): 
+            if aux1[i] < aux2[j]: 
+                arr[k] = aux1[i] 
+                i+=1
+            else: 
+                arr[k] = aux2[j] 
+                j+=1
+            k+=1
+          
+        while i < len(aux1): 
+            arr[k] = aux1[i] 
+            i+=1
+            k+=1
+          
+        while j < len(aux2): 
+            arr[k] = aux2[j] 
+            j+=1
+            k+=1
+
+def mostrar(array): 
+    for i in range(len(arr)):         
+        print(arr[i],end=" ") 
+ 
+
+
+archivo1=open("Archivo1.txt", "r")
+lineaArchivo1=archivo1.readline() 
+arr=lineaArchivo1.split(",")
+archivo1.close
+mostrar(arr) 
+mezclaNatural(arr) 
+mostrar(arr) 
+
+
+archivo3=open ("ArchivoSalida.txt", "w")
+archivo1=open("Archivo1.txt", "r")
+archivo2=open("Archivo2.txt", "r")
+repetir=True
+  
+lineaArchivo1=archivo1.readline() 
+lineaArchivo2=archivo2.readline()
+ 
+
+'''Se realizan comparaciones mientras la bandera no cambie'''
+while(repetir):
+    if(int(lineaArchivo1)<int(lineaArchivo2)):
+        archivo3.write(lineaArchivo1)
+        lineaArchivo1=archivo1.readline()
+        if(lineaArchivo1==""):
+            archivo3.write("\n")
+            archivo3.write(lineaArchivo2)
+            lineaArchivo2=archivo2.readline()
+            while(lineaArchivo2!=""):
+                archivo3.write(lineaArchivo2)
+                lineaArchivo2=archivo2.readline()
+            repetir=False
+    elif(int(lineaArchivo1)>int(lineaArchivo2)):
+        archivo3.write(lineaArchivo2)
+        lineaArchivo2=archivo2.readline()
+        if(lineaArchivo2==""):
+            archivo3.write("\n")
+            archivo3.write(lineaArchivo1)
+            lineaArchivo1=archivo1.readline()
+            while(lineaArchivo1!=""):
+                archivo3.write(lineaArchivo1)
+                lineaArchivo1=archivo1.readline()
+            repetir=False
+    else:
+        archivo3.write(lineaArchivo1)
+        archivo3.write(lineaArchivo2)
+        lineaArchivo1=archivo1.readline()
+        if(lineaArchivo1==""):
+            archivo3.write("\n")
+            archivo3.write(lineaArchivo2)
+            lineaArchivo2=archivo2.readline()
+            while(lineaArchivo2!=""):
+                archivo3.write(lineaArchivo2)
+                lineaArchivo2=archivo2.readline()
+            repetir=False
+        lineaArchivo2=archivo2.readline()
+        if(lineaArchivo2==""):
+            archivo3.write("\n")
+            archivo3.write(lineaArchivo1)
+            lineaArchivo1=archivo1.readline()
+            while(lineaArchivo1!=''):
+                archivo3.write(lineaArchivo1)
+                lineaArchivo1=archivo1.readline()
+            repetir=False
+archivo2.close
+archivo1.close
+print("Archivos combinados y ordenados correctamente")
+archivo3.close
 def ordenamientoQuickSort(lista):
     izq=[]
     centro=[]
